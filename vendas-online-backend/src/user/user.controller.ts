31b12fcf9a236863f2162a3a2c46f1a7698818b9
fabiohsalmeida <fabio.h.s.almeida@gmail.com>
2023-05-07 21:11:11 -0300

    Including dabatase connection, controller, entity and service of users

---
+++ vendas-online-backend/src/user/user.controller.ts
@@ -0,0 +1,22 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateUserDTO } from './dtos/createUser.dto';
+import { UserService } from './user.service';
+import { UserEntity } from './interfaces/user.entity';
+
+@Controller('user')
+export class UserController {
+
+    constructor(
+        private readonly userService: UserService
+    ) {}
+
+    @Post()
+    async createUser(@Body() createUser: CreateUserDTO): Promise<UserEntity> {
+        return this.userService.createUser(createUser)
+    }
+
+    @Get()
+    async getAllUsers(): Promise<UserEntity[]> {
+        return this.userService.getAllUsers()
+    }
+}
---
+++ vendas-online-backend/src/user/user.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateUserDTO } from './dtos/createUser.dto';
+import { UserEntity } from './interfaces/user.entity';
+import { hash } from 'bcrypt';
+
+@Injectable()
+export class UserService {
+    constructor(
+        @InjectRepository(UserEntity)
+        private readonly userRepository: Repository<UserEntity>
+    ) {}
+
+    async createUser(createUserDto: CreateUserDTO): Promise<UserEntity> {
+        const saltOrRounds = 10
+        const passwordHashed = await hash(createUserDto.password, saltOrRounds)
+        const user = {
+            ...createUserDto,
+            password: passwordHashed
+        }
+
+        return this.userRepository.save(user)
+    }
+
+    async getAllUsers(): Promise<UserEntity[]> {
+        return this.userRepository.find()
+    }
+}
